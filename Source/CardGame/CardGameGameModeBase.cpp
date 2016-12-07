420aa3b7ac10433580315f26841ba90859d67547
Kamikaze542 <gymnadude12@hotmail.com>
2016-12-07 13:50:54 -0500

    Player spawning is no longer random, this will allow us to associate the bases with their respective players

--- Source/CardGame/CardGameGameModeBase.cpp
+++ Source/CardGame/CardGameGameModeBase.cpp
@@ -9,18 +9,24 @@
 void ACardGameGameModeBase::PostLogin(APlayerController* NewPlayer)
 {
 	AActor* spawnLocation;
-	spawnLocation = ChoosePlayerStart(NewPlayer);
+	//spawnLocation = ChoosePlayerStart(NewPlayer);
+	//FString aString = "Astring";
+	PlayerList.Add(NewPlayer);
+	GEngine->AddOnScreenDebugMessage(-1, 1000.f, FColor::Green, FString::Printf(TEXT("NumPlayers: %d"), PlayerList.Num()));
+	spawnLocation = (PlayerList.Num() == 1) ? FindPlayerStart(NewPlayer, "Player1") : FindPlayerStart(NewPlayer, "Player2");
+	GEngine->AddOnScreenDebugMessage(-1, 1000.f, FColor::Green, FString::Printf(TEXT("NumPlayers: %d"), PlayerList.Num()));
 	GEngine->AddOnScreenDebugMessage(-1, 1000.f, FColor::Red, FString::Printf(TEXT("GameModeSpawnTransformInfo: %s, %s"), *spawnLocation->GetActorTransform().GetLocation().ToString(), *spawnLocation->GetActorTransform().GetRotation().Rotator().ToString()));
 	NewPlayer->SetInitialLocationAndRotation(spawnLocation->GetActorTransform().GetLocation(), spawnLocation->GetActorTransform().GetRotation().Rotator());
 	NewPlayer->ClientSetRotation(spawnLocation->GetActorTransform().GetRotation().Rotator());
-	PlayerList.Add(NewPlayer);
 	ACardGamePlayerController* pcRef = (ACardGamePlayerController*)NewPlayer;
-	if (PlayerList.Num() == 1)
+	if (spawnLocation->ActorHasTag("Player1"))
 	{
+		GEngine->AddOnScreenDebugMessage(-1, 1000.f, FColor::Blue, FString::Printf(TEXT("Player1")));
 		pcRef->SetP1(true);
 	}
-	else if (PlayerList.Num() == 2)
+	else if (spawnLocation->ActorHasTag("Player2"))
 	{
 		pcRef->SetP1(false);
+		GEngine->AddOnScreenDebugMessage(-1, 1000.f, FColor::Blue, FString::Printf(TEXT("Player2")));
 	}
 }
